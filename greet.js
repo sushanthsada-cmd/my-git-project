2451e61b324e635cea530fd8d972548dbc554a70
sushanthsada-cmd <265436098+sushanthsada-cmd@users.noreply.github.com>
2026-03-04 13:48:15 +0000

    Add thank you message to greet.js

--- greet.js
+++ greet.js
@@ -1,3 +1,5 @@
 // My first Git-tracked JavaScript file
 const name = process.argv[2] || "World";
 console.log(`Hello, ${name}! Welcome to Git!`);
+// Added: Professional greeting function
+console.log("Thank you for using Git!");
